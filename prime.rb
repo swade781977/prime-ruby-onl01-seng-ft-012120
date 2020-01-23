c3003c71dd1a0ac75ba64fc83ade3a4a447617e7
Chris Wade <swade781977@yahoo.com>
2020-01-23 21:10:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+require 'pry'
+def prime?(num)
+  result = true
+  if num <= 1
+    return false
+  end
+  array=*(2...num)
+
+  array.each do |a|
+    if num % a == 0
+      return false
+    end
+  end
+   result
+end
+  
+prime?(2)
