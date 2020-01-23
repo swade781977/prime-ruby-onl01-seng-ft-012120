106d814d058e357d4d2d84b76d06b99597f7e896
Chris Wade <swade781977@yahoo.com>
2020-01-23 20:39:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,16 @@
 
 def prime?(num)
+  if num <= 1
+      result = false
+  end
   array=*(2...num)
   array.each do |a|
-    if num <= 1
-      result = false
-    elsif num % a == 0
+    if num % a == 0
       result = false
     else
       result = true
     end
-    return result
   end
+   return result
 end
   
